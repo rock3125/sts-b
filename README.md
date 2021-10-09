c11b787323ac9ca1146bd217a2b3366d372ca5e3
Rock <rock@simsage.co.uk>
2021-10-09 22:05:10 +0100

    added code and readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# sts-b
-STS-B download and set up
+# STS-B
+
+roberta large sts-b
+
+## save packages to folder
+```
+pip download -d ./packages -r requirements.txt
+```
+
+## install packages for Ubuntu 20.04 from folder
+```
+pip install -r requirements.txt --find-links file://./packages
+```
+
---
+++ test-sts-b.py
@@ -0,0 +1,20 @@
+from sentence_transformers import SentenceTransformer, util
+import numpy as np
+
+model = SentenceTransformer('./model', device='cpu')
+# model = SentenceTransformer('stsb-roberta-large')
+# model.save('./model/')
+
+sentence1 = "I like Python because I can build AI applications"
+sentence2 = "I like Python because I can build A.I. applications"
+
+# encode sentences to get their embeddings
+embedding1 = model.encode(sentence1, convert_to_tensor=True)
+embedding2 = model.encode(sentence2, convert_to_tensor=True)
+print(embedding1.shape)
+
+# compute similarity scores of two embeddings
+cosine_scores = util.pytorch_cos_sim(embedding1, embedding2)
+print("Sentence 1:", sentence1)
+print("Sentence 2:", sentence2)
+print("Similarity score:", cosine_scores.item())

765a8dc5d05b32599802f42237807b55c5706e56
Rock <rock@simsage.co.uk>
2021-10-09 22:22:09 +0100

    added more models

--- download-model.py
+++ download-model.py
@@ -1,5 +1,7 @@
 from sentence_transformers import SentenceTransformer
 
 # model = SentenceTransformer('sentence-transformers/xlm-r-100langs-bert-base-nli-stsb-mean-tokens')
+# modal.save('./model-xlm/')
+
 model = SentenceTransformer('stsb-roberta-large')
-model.save('./model/')
+model.save('./model-roberta/')
